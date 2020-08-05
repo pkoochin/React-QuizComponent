a2bd2c59b0f00d0964ea10a4b098406767799001
Paul Koochin <pkoochin@sgi.sk.ca>
2020-08-05 14:32:04 -0600

    .

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
-import {QuizQuestion} from './QuizQuestion';
+import QuizQuestion from './QuizQuestion';
 
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component
 {
     constructor(props){
-        super(props);
-        this.state = { quiz_position: 1};
+        super(props)
+        this.state = { quiz_position: 1}
     }
     render(){
         return(
             <div>
-                <QuizQuestion />
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
             </div>
         );
     }
