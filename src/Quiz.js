b38a6dc7441df3d9a0ef473a2d369652b19d6086
Paul Koochin <pkoochin@sgi.sk.ca>
2020-08-05 14:21:25 -0600

    .

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component
     render(){
         return(
             <div>
-                <QuizQuestion/>
+                <QuizQuestion />
             </div>
         );
     }
