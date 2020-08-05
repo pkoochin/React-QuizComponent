be5a47b0a897414fda9e8f424d42311e9808d239
Paul Koochin <pkoochin@sgi.sk.ca>
2020-08-05 11:59:09 -0600

    End of Module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,15 +2,16 @@ import React, {Component} from 'react';
 
 let quizData = require('./quiz_data.json');
 
-class Quiz extends React.Component
+class Quiz extends Component
 {
     constructor(props){
         super(props);
+        this.state = { quiz_position: 1};
     }
     render(){
         return(
             <div>
-                Quiz
+                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
             </div>
         );
     }
